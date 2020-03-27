5fbc33b87314e753d10404e15953e49a5de4290f
Jacob Rozell <jrozell@myocv.com>
2020-03-27 12:23:30 -0500

    learn rotation animation

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -9,30 +9,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount: CGFloat = 1
+    @State private var animationAmount = 0.0
     
     var body: some View {
-        Button("Tap me") {
-            //self.animationAmount += 1
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                self.animationAmount += 360
+            }
         }
         .padding(50)
         .background(Color.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(Color.red)
-                .scaleEffect(animationAmount)
-                .opacity(Double(2 - animationAmount))
-                .animation(
-                    Animation.easeInOut(duration: 1)
-                        .repeatForever(autoreverses: false))
-        )
-        .onAppear {
-            self.animationAmount = 2
-        }
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 1))
     }
-
 }
 
 struct ContentView_Previews: PreviewProvider {
