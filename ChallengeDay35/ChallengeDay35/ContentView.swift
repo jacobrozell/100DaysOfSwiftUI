eec457b70445c824bc36abcc5df48ceb2f3553c2
Jacob Rozell <jrozell@myocv.com>
2020-06-09 09:39:02 -0500

    stuck on where to populate the questions

--- ChallengeDay35/ChallengeDay35/ContentView.swift
+++ ChallengeDay35/ChallengeDay35/ContentView.swift
@@ -8,7 +8,9 @@
 
 import SwiftUI
 
-struct Question {
+struct Question: Identifiable {
+    var id = UUID()
+    
     var text: String = ""
     var answer: Int = 0
 }
@@ -40,7 +42,6 @@ struct SettingsForm: View {
                     Stepper(value: $tableToUse, in: 2...12) {
                         Text("Table: \(tableToUse)")
                     }
-                            
                 }
                 
                 Section(header:
@@ -57,22 +58,12 @@ struct SettingsForm: View {
                     Text("Questions: \(questionChoices[selectedIndex])")
                 }
             }
-            StartButton(tableToUse: $tableToUse, selectedIndex: $selectedIndex)
-        }
-    }
-}
-
-struct StartButton: View {
-    @Binding var tableToUse: Int
-    @Binding var selectedIndex: Int
-    let questionChoices = ["5", "10", "15", "20"]
-    
-    var body: some View {
-        VStack {
-            NavigationLink(destination: GameView()) {
+            
+            NavigationLink(destination: GameView(tableToUse: tableToUse, selectedIndex: selectedIndex)) {
                 HStack {
                     Text("Start Game!")
-                }.padding(10.0)
+                }
+                .padding(10.0)
                 .overlay(
                     RoundedRectangle(cornerRadius: 16.0)
                         .stroke(lineWidth: 1.0)
@@ -80,29 +71,64 @@ struct StartButton: View {
                 )
             }
         }
-        .onDisappear() {
-            self.generateQuestions()
-        }
-    }
-    
-    func generateQuestions() {
-        var questions: [Question] = []
-        let questionAmount: Int = Int(questionChoices[selectedIndex]) ?? 5
-        for i in 1...questionAmount {
-            questions.append(Question(text: "\(tableToUse) X \(i)", answer: i * tableToUse))
-        }
-        
-        #warning("Remove this")
-        for i in questions {
-            print(i)
-        }
     }
 }
 
 // MARK: Game State
 struct GameView: View {
+    @State private var questions: [Question] = []
+    var tableToUse: Int
+    var selectedIndex: Int
+    let questionChoices = ["5", "10", "15", "20"]
+    
+    @State private var currentQuestion = 0
+    @State private var correctIndex = 0
+    
+    init(tableToUse: Int, selectedIndex: Int) {
+        self.tableToUse = tableToUse
+        self.selectedIndex = selectedIndex
+        questions = self.generateQuestions(tableValue: tableToUse, index: selectedIndex)
+    }
+    
     var body: some View {
-        Text("GAMEVIEW")
+        VStack {
+            if !questions.isEmpty {
+                Text("\(questions[currentQuestion].text)")
+                
+                HStack(spacing: 50) {
+                    ForEach(1...3, id: \.self) { index in
+                        Button(action: {
+                            index == self.correctIndex ? self.correct() : self.incorrect()
+                        }) {
+                            Text("\(index == self.correctIndex ? self.questions[self.currentQuestion].answer :  self.questions[Int.random(in: 0...self.questionChoices.count)].answer) ")
+                        }
+                    }
+                }
+                
+                Spacer()
+            }
+        }
+    }
+    
+    func generateQuestions(tableValue: Int, index: Int) -> [Question] {
+        questions = []
+        let questionAmount: Int = Int(questionChoices[index]) ?? 5
+        for i in 1...questionAmount {
+            questions.append(Question(text: "\(tableToUse) X \(i)", answer: i * tableToUse))
+        }
+        return questions.shuffled()
+    }
+    
+    func askQuestion() {
+        correctIndex = Int.random(in: 1...3)
+    }
+    
+    func correct() {
+        
+    }
+    
+    func incorrect() {
+        
     }
 }
 
