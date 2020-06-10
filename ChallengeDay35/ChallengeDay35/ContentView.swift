a788740173eee2bbda22010ddaf6100ade2eaca0
Jacob Rozell <jrozell@myocv.com>
2020-06-10 15:48:24 -0500

    finish challengeday35 base

--- ChallengeDay35/ChallengeDay35/ContentView.swift
+++ ChallengeDay35/ChallengeDay35/ContentView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 
 struct Question: Identifiable {
     var id = UUID()
-    
     var text: String = ""
     var answer: Int = 0
 }
@@ -21,28 +20,42 @@ struct ContentView: View {
         NavigationView {
             SettingsForm()
                 
+
         .navigationBarTitle("Multiplication")
         }
     }
 }
 
 struct SettingsForm: View {
+
+    enum QuestionOption {
+        case correct
+        case incorrect
+    }
+
     let questionChoices = ["5", "10", "15", "20"]
+    let praises = ["Good job!", "Fantastic!", "Wonderful!", "Awesome!", "Spectacular!", "Cool!", "Wow!", "Way to go!"]
     @State private var tableToUse = 2
     @State private var selectedIndex = 0
-    
+
     @State private var questions: [Question] = []
-    @State private var currentQuestion = -1     // Starts at 0 in askQuestion
-    @State private var correctIndex = 0
-    
+    @State private var currentQuestion = 0     // Starts at 0 in askQuestion
+
+    @State private var makeButtons = true
     @State private var inSettings = true
     let questionRange = 1...3
-    
-    @State private var gameOverAlert = false
-    
+
+    @State private var answerText = ""
+
+    @State private var showAlert = false
+    @State private var alertText = ""
+    @State private var alertMessage = ""
+    @State private var alertButtonText = ""
+    @State private var alertState: QuestionOption = .incorrect
+
     @State private var questionsCorrect = 0
     @State private var maxQuestionsAsked = 0
-    
+
     var body: some View {
         VStack {
             if inSettings {
@@ -50,129 +63,132 @@ struct SettingsForm: View {
                     Section(header:
                         Text("Choose Table")
                             .font(.headline)) {
-                                
+
                                 Stepper(value: $tableToUse, in: 2...12) {
                                     Text("Table: \(tableToUse)")
                                 }
                     }
-                    
+
                     Section(header:
                         Text("Choose Question Amount")
                             .font(.headline)) {
-                                
+
                                 Picker("Questions", selection: $selectedIndex) {
                                     ForEach(0 ..< questionChoices.count) {
                                         Text("\(self.questionChoices[$0])")
                                     }
                                 }
                                 .pickerStyle(SegmentedPickerStyle())
-                                
+
                                 Text("Questions: \(questionChoices[selectedIndex])")
                     }
                 }
-                
+
                 GameButton(action: {
                     self.startGame()
                 }, text: "Start Game")
+                    .padding()
             }
             else {
-                ZStack {
-                    //LinearGradient(gradient: Gradient(colors: [.red, .blue, .orange]), startPoint: .center, endPoint: .topTrailing)
-                    
-                    VStack {
-                        Text("\(questions[currentQuestion].text)")
-                            .frame(width: 1000, height: 100, alignment: .center)
-                            .clipShape(Capsule(style: .circular))
-                            .font(.largeTitle)
-                            .background(Color.blue)
-                            .foregroundColor(Color.white)
-                        
-                        HStack {
-                            Text("Question: \(currentQuestion)")
-                            Text("Score: \(100)")
-                        }
-                            
-                        Spacer()
-                        
-                        HStack(spacing: 65) {
-                            ForEach(questionRange, id: \.self) { index in
+                VStack(spacing: 50) {
+                    Section(header:
+                        Text("Question: \(currentQuestion) / \(maxQuestionsAsked)")
+                            .font(.largeTitle)) {
+                                Text("\(questions[currentQuestion-1].text)")
+                                    .font(.largeTitle)
+                    }
 
-                                GameButton(action: {
-                                    index == self.correctIndex ? self.correct() : self.incorrect()
-                                    
-                                }, text: "\(index == self.correctIndex ? self.questions[self.currentQuestion].answer :  self.generateWrongAnswer(correctIndex: self.correctIndex))")
-                            }
-                        }
+                    HStack {
+                        TextField("Answer:", text: $answerText)
+                            .keyboardType(.numberPad)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding()
+
+                        GameButton(action: {
+                            self.checkAnswer()
+                        }, text: "Check Answer")
+                            .padding()
                     }
-                    // GameOver Alert - calls gameOver()
-                    .alert(isPresented: $gameOverAlert) {
-                        Alert(title: Text("Game Over!"), message: Text("You got \(self.questionsCorrect)/\(self.maxQuestionsAsked) questions correct!"), dismissButton: .default(Text("Play Again!")))
-                    }
+
+                    Spacer()
+                }.onTapGesture {
+                    UIApplication.shared.endEditing()
                 }
+
+                .alert(isPresented: $showAlert) {
+                    Alert(title: Text("\(alertText)"), message: Text("\(alertMessage)"), dismissButton: .default(Text("\(alertButtonText)")) {
+
+                        switch self.alertState {
+                        case .correct:
+                            self.answerText = ""
+                            self.askQuestion()
+                        case .incorrect:
+                            return
+                        }
+                        self.showAlert = false
+                        self.makeButtons = false
+                        })
+                }
+                .animation(.default)
             }
         }
     }
-    
+
     func startGame() {
         // Generate Questions
         questions = []
         maxQuestionsAsked = Int(questionChoices[selectedIndex]) ?? 5
-        
-        for i in 1...maxQuestionsAsked {
+
+        for i in 1...maxQuestionsAsked+1 {
             questions.append(Question(text: "\(tableToUse) X \(i)", answer: i * tableToUse))
         }
         questions.shuffle()
-        
+
         // Start Game
         askQuestion()
         inSettings = false
     }
-    
+
+    func checkAnswer() {
+        if Int(answerText) == questions[currentQuestion-1].answer {
+            print("Correct")
+            alertText = "Correct"
+            alertMessage = praises.randomElement() ?? "Good Job!"
+            alertButtonText = "Next Question!"
+            alertState = .correct
+
+        } else {
+            print("incorrect")
+            alertText = "Incorrect!"
+            alertMessage = "Try again?"
+            alertButtonText = "Okay!"
+            alertState = .incorrect
+        }
+        showAlert = true
+    }
+
     func askQuestion() {
-        if questions.count-1 == currentQuestion {
+        currentQuestion += 1
+
+        if currentQuestion >= questions.count {
             gameOver()
             return
         }
-        
-        correctIndex = Int.random(in: questionRange)
-        currentQuestion += 1
     }
-    
-    func correct() {
-        print("correct")
-        questionsCorrect += 1
-        askQuestion()
-    }
-    
-    func incorrect() {
-        print("incorrect")
-    }
-    
+
     func gameOver() {
         print("game over!")
-        
-        currentQuestion = -1
+
+        currentQuestion = 0
         self.inSettings = true
-    }
-    
-    func generateWrongAnswer(correctIndex: Int) -> Int {
-        let correctAnswer = self.questions[self.currentQuestion].answer
-        var wrongAnswer = self.questions[Int.random(in: 0...self.questionChoices.count)].answer
-        
-        // Make sure we are generating a non-duplicate wrong answer
-        while wrongAnswer == correctAnswer {
-            wrongAnswer = self.questions[Int.random(in: 0...self.questionChoices.count)].answer
-        }
-        
-        return wrongAnswer
     }
 }
 
-// MARK: Views
+// MARK: - CustomViews
 struct GameButton: View {
     var action: () -> ()
     var text: String
-    
+
     var body: some View {
         Button(action: {
             self.action()
@@ -189,6 +205,13 @@ struct GameButton: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+// MARK: - Extensions
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
