5c4ec21fe4903fefd256945a609bcdd11318a97c
Jacob Rozell <jrozell@myocv.com>
2020-06-16 11:47:48 -0500

    finish iExpense

--- iExpense/iExpense/Expense.swift
+++ iExpense/iExpense/Expense.swift
@@ -10,12 +10,34 @@ import Foundation
 
 struct ExpenseItem: Identifiable, Codable {
     var id = UUID()
-
     let name: String
     let type: String
     let amount: Int
 }
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+
+    init() {
+        // Grab cached data
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+
+        // Init to empty
+        self.items = []
+    }
 }
