7cd42b6a9728294f8f8531e6b27a60fc5ab91d23
Jacob Rozell <jrozell@myocv.com>
2020-06-23 10:03:52 -0500

    added cool zoom-in/zoom-out effect

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -8,36 +8,58 @@
 
 import SwiftUI
 
-struct Trapezoid: Shape {
-    var insetAmount: CGFloat
-    var animatableData: CGFloat {
-        get { insetAmount }
-        set { self.insetAmount = newValue }
+struct CheckerBoard: Shape {
+    var rows: Int
+    var columns: Int
+
+    public var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatableData(Double(rows), Double(columns))
+        }
+
+        set {
+            self.rows = Int(newValue.first)
+            self.columns = Int(newValue.second)
+        }
     }
 
     func path(in rect: CGRect) -> Path {
         var path = Path()
 
-        path.move(to: CGPoint(x: 0, y: rect.maxY))
-        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
+
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
 
         return path
     }
 }
 
+
 struct ContentView: View {
-    @State private var insetAmount: CGFloat = 50
+    @State private var rows = 4
+    @State private var columns = 4
+    @State private var zooming = true
+
     var body: some View {
-        Trapezoid(insetAmount: insetAmount)
-            .frame(width: 200, height: 100)
+        CheckerBoard(rows: rows, columns: columns)
             .onTapGesture {
-                withAnimation {
-                    self.insetAmount = CGFloat.random(in: 10...90)
+                withAnimation(.linear(duration: 3)) {
+                    self.rows = self.zooming ? 100 : 4
+                    self.columns = self.zooming ? 100 : 4
+                    self.zooming.toggle()
                 }
-            }
+        }
     }
 }
 
